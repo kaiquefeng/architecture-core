87214dfa0cf65cabd6612c8f08357c2c826170b6
kaiquefeng <kaiquefeng1@gmail.com>
2023-12-29 16:59:47 -0300

    chore: remove rename components

--- src/format-files.ts
+++ src/format-files.ts
@@ -4,32 +4,6 @@ import fs from 'fs';
 import path from 'path';
 import { EXCLUDED_PATHS } from './constants/excluded-paths.js';
 
-function renameComponents(directory: string) {
-  fs.readdir(directory, { withFileTypes: true }, (err, files) => {
-    for (const file of files) {
-      const fullPath = path.join(directory, file.name);
-      const isExcluded = EXCLUDED_PATHS.some((excludedPath) => file.name.includes(excludedPath));
-
-      if (file.isDirectory() && !isExcluded) {
-        const components = fs.readdirSync(fullPath);
-
-        components.forEach((component) => {
-          if (component.endsWith('.tsx')) {
-            const componentName = component.replace('.tsx', '');
-            const componentNameFormatted = ConvertToKebabCase(componentName);
-
-            const newDir = path.join(fullPath, componentNameFormatted);
-
-            fs.mkdirSync(newDir);
-            fs.renameSync(path.join(fullPath, component), path.join(newDir, `${componentNameFormatted}.tsx`));
-            fs.writeFileSync(path.join(newDir, 'index.ts'), `export * from './${componentNameFormatted}';\n`);
-          }
-        });
-      }
-    }
-  });
-}
-
 function renameDirectories(directory: string) {
   fs.readdir(directory, { withFileTypes: true }, (err, files) => {
     for (const file of files) {
@@ -47,8 +21,6 @@ function renameDirectories(directory: string) {
       }
     }
   });
-
-  renameComponents(directory);
 }
 
 renameDirectories(process.cwd());
